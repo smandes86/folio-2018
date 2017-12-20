0cec8fba8ff761f6ecfb4522c45f820f4e89c1b7
SMandes <sam620@its26191m2.its.rutgers.edu>
2017-12-20 09:50:01 -0500

    Add content and updates

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
 // array with texts to type in typewriter
-var dataText = ["User Experience","Design.", "Development.", "Steve Mandes"];
+var dataText = ["User Experience.","Design.", "Development.", "Steve Mandes"];
 
 // type one text in the typwriter
 // keeps calling itself until the text is finished
